fda89582e78f7e03e6480133814f4313868410f8
tapuks <david_berdiell@hotmail.com>
2021-05-29 11:03:18 +0000

    table user and post

--- src/models.py
+++ src/models.py
@@ -8,23 +8,23 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    follower = Column(Integer(50))
+    following = Column(Integer(50))
+    description = Column (String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'posts'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    like = Column(Integer(250))
+    saves = Column(Integer(250))
+    description = Column(String(999))
+    
 
     def to_dict(self):
         return {}
